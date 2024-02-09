ab47bf6a4f7e661fb37230fd8a92f4d4bf368df9
tetsuya-azami <zqianjian35@gmail.com>
2024-02-10 06:50:22 +0900

    first commit

---
+++ merchant-reception/src/main/kotlin/com/example/merchantreception/MerchantReceptionApplication.kt
@@ -0,0 +1,11 @@
+package com.example.merchantreception
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MerchantReceptionApplication
+
+fun main(args: Array<String>) {
+	runApplication<MerchantReceptionApplication>(*args)
+}
---
+++ order-processing/src/main/kotlin/com/example/orderprocessing/OrderProcessingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.orderprocessing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderProcessingApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderProcessingApplication>(*args)
+}
