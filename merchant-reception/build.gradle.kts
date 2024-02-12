c33e966c69b32f74a478c9b9820457e8f8a45c10
tetsuya-azami <zqianjian35@gmail.com>
2024-02-12 17:58:26 +0900

    introduce openapi generator

--- merchant-reception/build.gradle.kts
+++ merchant-reception/build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.22"
 	kotlin("plugin.spring") version "1.9.22"
+	id ("org.openapi.generator") version "6.6.0"
 }
 
 group = "com.example"
@@ -21,6 +23,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web-services")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.openapitools:openapi-generator-gradle-plugin:7.1.0")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
@@ -34,4 +37,17 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+val domainName = "com.example.merchant-reception"
+
+openApiGenerate {
+	generatorName = "kotlin-spring"
+	inputSpec = "$rootDir/specs/openapi.yaml"
+	outputDir = "$buildDir/generated"
+	apiPackage = "${domainName}.api"
+	invokerPackage = "${domainName}.api.invoker"
+	modelPackage = "${domainName}.model"
+	configOptions = mapOf("useSpringBoot3" to "true")
+	globalProperties = mapOf("modelDocs" to "false")
 }
