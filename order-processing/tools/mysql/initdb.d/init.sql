470816c0ded11b3040c4cf8f2731cfd9ec7a791d
tetsuya-azami <zqianjian35@gmail.com>
2024-03-14 09:33:14 +0900

    fix database column type

--- order-processing/tools/mysql/initdb.d/init.sql
+++ order-processing/tools/mysql/initdb.d/init.sql
@@ -1,12 +1,11 @@
 CREATE TABLE orders(
-        order_id integer PRIMARY KEY AUTO_INCREMENT,
-        chain_id integer NOT NULL,
-        shop_id integer NOT NULL,
-        user_id integer NOT NULL,
+        order_id bigint PRIMARY KEY AUTO_INCREMENT,
+        chain_id bigint NOT NULL,
+        shop_id bigint NOT NULL,
+        user_id bigint NOT NULL,
         payment_method varchar(100) NOT NULL,
-        delivery_address_id integer NOT NULL,
+        delivery_address_id bigint NOT NULL,
         delivery_type varchar(100) NOT NULL CHECK (delivery_type IN ('immediate', 'specified')),
-        delivery_method_id integer NOT NULL,
         delivery_charge decimal NOT NULL,
         non_taxed_total_price decimal NOT NULL,
         tax decimal NOT NULL,
@@ -17,9 +16,9 @@ CREATE TABLE orders(
 );
 
 CREATE TABLE items(
-        item_id integer PRIMARY KEY AUTO_INCREMENT,
-        chain_id integer NOT NULL,
-        shop_id integer NOT NULL,
+        item_id bigint PRIMARY KEY AUTO_INCREMENT,
+        chain_id bigint NOT NULL,
+        shop_id bigint NOT NULL,
         name varchar(250) NOT NULL,
         price decimal NOT NULL,
         description varchar(500) NOT NULL,
@@ -28,8 +27,8 @@ CREATE TABLE items(
 );
 
 CREATE TABLE order_items(
-        order_id integer NOT NULL,
-        item_id integer NOT NULL,
+        order_id bigint NOT NULL,
+        item_id bigint NOT NULL,
         quantity integer NOT NULL,
         created_at datetime NOT NULL,
         updated_at datetime NOT NULL,
@@ -39,7 +38,7 @@ CREATE TABLE order_items(
 );
 
 CREATE TABLE attributes(
-        attribute_id integer PRIMARY KEY AUTO_INCREMENT,
+        attribute_id bigint PRIMARY KEY AUTO_INCREMENT,
         name varchar(250) NOT NULL,
         value varchar(250) NOT NULL,
         created_at datetime NOT NULL,
@@ -47,8 +46,8 @@ CREATE TABLE attributes(
 );
 
 CREATE TABLE item_attributes(
-        item_id integer NOT NULL,
-        attribute_id integer NOT NULL,
+        item_id bigint NOT NULL,
+        attribute_id bigint NOT NULL,
         created_at datetime NOT NULL,
         updated_at datetime NOT NULL,
         PRIMARY KEY (item_id, attribute_id),
@@ -57,21 +56,15 @@ CREATE TABLE item_attributes(
 );
 
 CREATE TABLE order_item_attributes(
-        order_id integer NOT NULL,
-        item_id integer NOT NULL,
-        attribute_id integer NOT NULL,
+        order_id bigint NOT NULL,
+        item_id bigint NOT NULL,
+        attribute_id bigint NOT NULL,
         created_at datetime NOT NULL,
         updated_at datetime NOT NULL,
         PRIMARY KEY (order_id, item_id, attribute_id),
         FOREIGN KEY (order_id) REFERENCES orders(order_id),
         FOREIGN KEY (item_id) REFERENCES items(item_id),
         FOREIGN KEY (attribute_id) REFERENCES attributes(attribute_id)
-);
-
-CREATE TABLE delivery_methods(
-        name varchar(250) PRIMARY KEY,
-        created_at datetime NOT NULL,
-        updated_at datetime NOT NULL
 );
 
 CREATE TABLE payment_methods(
