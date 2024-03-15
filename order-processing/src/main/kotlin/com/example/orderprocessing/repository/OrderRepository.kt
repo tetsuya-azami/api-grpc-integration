e5486f0bbcfc5d89a9993f02a03051547ca23972
tetsuya-azami <zqianjian35@gmail.com>
2024-03-16 07:02:41 +0900

    fix to use regenerated mybatis codes

--- order-processing/src/main/kotlin/com/example/orderprocessing/repository/OrderRepository.kt
+++ order-processing/src/main/kotlin/com/example/orderprocessing/repository/OrderRepository.kt
@@ -1,17 +1,17 @@
 package com.example.orderprocessing.repository
 
 import com.example.orderprocessing.model.order.Order
-import com.example.orderprocessing.repository.entity.generated.Orders
-import com.example.orderprocessing.repository.mapper.generated.OrdersMapper
+import com.example.orderprocessing.repository.entity.generated.OrdersBase
+import com.example.orderprocessing.repository.mapper.generated.OrdersBaseMapper
 import com.example.orderprocessing.repository.mapper.generated.insert
 import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
 
 @Repository
-class OrderRepository(private val ordersMapper: OrdersMapper) {
+class OrderRepository(private val ordersMapper: OrdersBaseMapper) {
     fun createOrder(order: Order) {
         val now = LocalDateTime.now()
-        val row = Orders()
+        val row = OrdersBase()
         row.chainId = order.chainId
         row.shopId = order.shopId
         row.userId = order.user.userId
