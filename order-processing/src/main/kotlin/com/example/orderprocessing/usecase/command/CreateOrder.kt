44c0fd2fffe642c6d44b3ecc006bc07368c257a3
tetsuya-azami <zqianjian35@gmail.com>
2024-03-19 07:47:00 +0900

    add OrderItem creation logic to Service

--- order-processing/src/main/kotlin/com/example/orderprocessing/usecase/command/CreateOrder.kt
+++ order-processing/src/main/kotlin/com/example/orderprocessing/usecase/command/CreateOrder.kt
@@ -2,19 +2,24 @@ package com.example.orderprocessing.usecase.command
 
 import com.example.orderprocessing.model.order.Order
 import com.example.orderprocessing.model.order.OrderId
+import com.example.orderprocessing.repository.order.OrderItemRepository
 import com.example.orderprocessing.repository.order.OrderRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
 
 @Service
-class CreateOrder(private val orderRepository: OrderRepository) {
+class CreateOrder(
+    private val orderRepository: OrderRepository,
+    private val orderItemRepository: OrderItemRepository
+) {
     @Transactional
     fun execute(order: Order): OrderId {
         val now = LocalDateTime.now()
-        orderRepository.createOrder(order, now)
 
-        // TODO: 作成したorderのIDを返す
-        return OrderId(1)
+        val orderId = orderRepository.createOrder(order, now)
+        orderItemRepository.createOrderItem(orderId, order.orderItems, now)
+
+        return orderId
     }
 }
