f203a9226f6a664188cb4c5af9b333ddabd78897
tetsuya-azami <zqianjian35@gmail.com>
2024-03-20 12:26:47 +0900

    fix submodule

--- order-reception/grpc-interface/build/scripts/protoc-gen-grpc-kotlin-1.3.0-jdk8-generateProto-trampoline.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec '/opt/homebrew/Cellar/openjdk@17/17.0.9/libexec/openjdk.jdk/Contents/Home/bin/java' -jar '/Users/tetsu/.gradle/caches/modules-2/files-2.1/io.grpc/protoc-gen-grpc-kotlin/1.3.0/cb20cb447ec7c77d59d118580db3630c92b9b0bb/protoc-gen-grpc-kotlin-1.3.0-jdk8.jar' "$@"
