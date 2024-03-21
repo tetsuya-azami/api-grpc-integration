1e56ad2533cd63bc7e64702fef46f7d9a761f0f4
tetsuya-azami <zqianjian35@gmail.com>
2024-03-21 09:04:00 +0900

    fix OrderRepositoryTest

--- order-processing/src/test/kotlin/com/example/orderprocessing/repository/order/OrderRepositoryTest.kt
+++ order-processing/src/test/kotlin/com/example/orderprocessing/repository/order/OrderRepositoryTest.kt
@@ -1,9 +1,12 @@
 package com.example.orderprocessing.repository.order
 
-import com.example.orderprocessing.model.order.*
+import com.example.grpcinterface.proto.OrderOuterClass
+import com.example.grpcinterface.proto.OrderOuterClass.OrderCreationRequest
+import com.example.orderprocessing.model.order.Order
 import com.example.orderprocessing.repository.entity.generated.OrdersBase
 import com.example.orderprocessing.repository.mapper.generated.OrdersBaseMapper
 import com.example.orderprocessing.repository.mapper.generated.select
+import com.google.protobuf.Timestamp
 import io.mockk.every
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
@@ -15,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.jdbc.Sql
 import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 @SpringBootTest
 class OrderRepositoryTest @Autowired constructor(
@@ -42,51 +46,44 @@ class OrderRepositoryTest @Autowired constructor(
     )
     fun insertTest() {
         // Given
-        val order = createTestOrder()
+        val order = OrderOuterClass.Order.newBuilder()
+            .setChain(OrderOuterClass.Chain.newBuilder().setId(1).build())
+            .setShop(OrderOuterClass.Shop.newBuilder().setId(1).build())
+            .setDelivery(
+                OrderOuterClass.Delivery.newBuilder()
+                    .setType(OrderOuterClass.Delivery.Type.IMMEDIATE)
+                    .setAddressId(1)
+                    .build()
+            )
+            .setUser(
+                OrderOuterClass.User.newBuilder()
+                    .setId(1)
+                    .setBlackLevel(OrderOuterClass.User.BlackLevel.LOW)
+                    .build()
+            )
+            .setPayment(
+                OrderOuterClass.Payment.newBuilder()
+                    .setPaymentMethod(OrderOuterClass.Payment.PaymentMethod.CASH)
+                    .setDeliveryCharge(350)
+                    .setNonTaxedTotalPrice(1000)
+                    .setTax(135)
+                    .setTaxedTotalPrice(1485)
+                    .build()
+            )
+            .setTime(Timestamp.newBuilder().setSeconds(now.toEpochSecond(ZoneOffset.of("+09:00"))).build())
+        val orderCreationRequest = OrderCreationRequest.newBuilder().setOrder(order).build()
+        val orderModel = Order.fromOrderCreationRequest(orderCreationRequest)
 
         // When
-        val insertedOrderId = orderRepository.createOrder(order, now)
-        assertThat(insertedOrderId).isEqualTo(OrderId(1))
+        val insertedOrderId = orderRepository.registerOrder(orderModel, now)
+        assertThat(insertedOrderId).isEqualTo(orderModel.orderId)
 
         // Then
         val insertedOrderList = ordersBaseMapper.select {}
         assertThat(insertedOrderList.size).isEqualTo(1)
         val insertedOrder = insertedOrderList.first()
 
-        assert_登録された注文情報が正しいこと(insertedOrder, order)
-    }
-
-    private fun createTestOrder(): Order {
-        val orderItems = createTestOrderItems()
-        val delivery = Delivery.createTestDelivery(type = DeliveryType.IMMEDIATE, addressId = 1)
-        val user = User.createTestUser(userId = 1, blackLevel = BlackLevel.LOW)
-        val payment = Payment.createTestPayment(
-            paymentMethodType = PaymentMethodType.CASH,
-            deliveryCharge = 350,
-            nonTaxedTotalPrice = 1000,
-            tax = 135,
-            taxedTotalPrice = 1485
-        )
-
-        return Order.createTestOrder(
-            orderId = OrderId(1),
-            orderItems = orderItems,
-            chainId = 1,
-            shopId = 1,
-            delivery = delivery,
-            user = user,
-            payment = payment,
-            time = now
-        )
-    }
-
-    private fun createTestOrderItems(): List<OrderItem> {
-        val item1Attributes = listOf(OrderItemAttribute(1), OrderItemAttribute(2))
-        val orderItem1 = OrderItem(1, 100, 2, item1Attributes)
-        val item2Attributes = listOf(OrderItemAttribute(1), OrderItemAttribute(3))
-        val orderItem2 = OrderItem(2, 200, 1, item2Attributes)
-
-        return listOf(orderItem1, orderItem2)
+        assert_登録された注文情報が正しいこと(insertedOrder, orderModel)
     }
 
     private fun assert_登録された注文情報が正しいこと(
