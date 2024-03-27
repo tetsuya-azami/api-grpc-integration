435f0325de4ec723d7acdb30e81a4c9f31daa6e1
tetsuya-azami <zqianjian35@gmail.com>
2024-03-27 09:06:05 +0900

    add log

--- order-processing/src/main/kotlin/com/example/orderprocessing/controller/OrderController.kt
+++ order-processing/src/main/kotlin/com/example/orderprocessing/controller/OrderController.kt
@@ -5,14 +5,25 @@ import com.example.grpcinterface.proto.OrderServiceGrpcKt
 import com.example.orderprocessing.model.order.Order
 import com.example.orderprocessing.usecase.command.RegisterOrder
 import net.devh.boot.grpc.server.service.GrpcService
+import org.slf4j.LoggerFactory
 
 @GrpcService
 @Suppress("unused")
-class OrderController(private val registerOrder: RegisterOrder) : OrderServiceGrpcKt.OrderServiceCoroutineImplBase() {
+class OrderController(
+    private val registerOrder: RegisterOrder
+) : OrderServiceGrpcKt.OrderServiceCoroutineImplBase() {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     override suspend fun createOrder(request: OrderOuterClass.OrderCreationRequest): OrderOuterClass.OrderCreationResponse {
+        logger.info("receive request: $request")
+
         val order = Order.fromOrderCreationRequest(request)
         val orderId = registerOrder.execute(order)
 
-        return OrderOuterClass.OrderCreationResponse.newBuilder().setId(orderId.value).build()
+        val response = OrderOuterClass.OrderCreationResponse.newBuilder().setId(orderId.value).build()
+
+        logger.info("send response: $response")
+        
+        return response
     }
 }
--- order-processing/src/main/kotlin/com/example/orderprocessing/usecase/command/RegisterOrder.kt
+++ order-processing/src/main/kotlin/com/example/orderprocessing/usecase/command/RegisterOrder.kt
@@ -3,6 +3,7 @@ package com.example.orderprocessing.usecase.command
 import com.example.orderprocessing.model.order.Order
 import com.example.orderprocessing.model.order.OrderId
 import com.example.orderprocessing.repository.order.OrderRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
@@ -11,10 +12,16 @@ import java.time.LocalDateTime
 class RegisterOrder(
     private val orderRepository: OrderRepository
 ) {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     @Transactional
     fun execute(order: Order): OrderId {
-        val now = LocalDateTime.now()
+        logger.info("注文登録処理開始")
 
-        return orderRepository.registerOrder(order, now)
+        val now = LocalDateTime.now()
+        val registeredOrderId = orderRepository.registerOrder(order, now)
+
+        logger.info("注文登録処理終了")
+        return registeredOrderId
     }
 }
