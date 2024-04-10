8ccb2ffbb960b510a217a4d05dd93df43d9f8791
tetsuya-azami <zqianjian35@gmail.com>
2024-04-11 08:44:28 +0900

    refactor test using Mockk Annotations

--- order-processing/src/test/kotlin/com/example/orderprocessing/presentation/order/OrderControllerTest.kt
+++ order-processing/src/test/kotlin/com/example/orderprocessing/presentation/order/OrderControllerTest.kt
@@ -6,24 +6,29 @@ import com.example.orderprocessing.domain.model.OrderId
 import com.example.orderprocessing.helper.OrderTestHelper
 import com.example.orderprocessing.usecase.command.RegisterOrder
 import io.mockk.every
-import io.mockk.mockk
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
 import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(MockKExtension::class)
 class OrderControllerTest {
 
+    @InjectMockKs
     private lateinit var orderController: OrderController
+
+    @MockK
+    private lateinit var registerOrder: RegisterOrder
     private lateinit var testOrderId: OrderId
 
     @BeforeEach
     fun setUp() {
         testOrderId = OrderId.new()
-        val mockedRegisterOrder = mockk<RegisterOrder>()
-        every { mockedRegisterOrder.execute(any()) } returns testOrderId
-
-        orderController = OrderController(mockedRegisterOrder)
+        every { registerOrder.execute(any()) } returns testOrderId
     }
 
     @Test
