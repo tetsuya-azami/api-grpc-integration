c8d0d52b5bd4febaa40c3c11f1454bb52767d56a
tetsuya-azami <zqianjian35@gmail.com>
2024-04-15 06:23:12 +0900

    fix not to use Validator

--- order-reception/src/main/kotlin/com/example/orderreception/presentation/order/OrderController.kt
+++ order-reception/src/main/kotlin/com/example/orderreception/presentation/order/OrderController.kt
@@ -5,26 +5,25 @@ import com.example.orderreception.openapi.api.OrderApi
 import com.example.orderreception.openapi.model.CreateOrderRequest
 import com.example.orderreception.openapi.model.OrderCreationSuccessResponse
 import com.example.orderreception.openapi.model.OrderCreationSuccessResponseData
+import com.github.michaelbull.result.getOrElse
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class OrderController(
-    private val orderValidator: OrderValidator
-) : OrderApi {
+class OrderController : OrderApi {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun createOrder(createOrderRequest: CreateOrderRequest): ResponseEntity<OrderCreationSuccessResponse> {
         logger.info("Create order request: $createOrderRequest")
 
-        val validationErrors = orderValidator.isOrderValid(createOrderRequest)
-        if (validationErrors.isNotEmpty()) {
-            logger.warn("注文情報のバリデーションに失敗しました。: $${validationErrors}")
-            throw OrderReceptionIllegalArgumentException(validationErrors = validationErrors)
-        }
+        val orderParam = OrderParam.fromOpenApi(order = createOrderRequest.order)
+            .getOrElse { errors ->
+                logger.warn("注文情報のバリデーションに失敗しました。: $${errors}")
+                throw OrderReceptionIllegalArgumentException(validationErrors = errors)
+            }
 
         logger.info("Order created successfully: $createOrderRequest")
         return ResponseEntity(
