f96768786973796378b88e0878ea0045d6f2f8ee
tetsuya-azami <zqianjian35@gmail.com>
2024-04-16 06:37:48 +0900

    organize order-reception tables

--- order-reception/tools/mysql/initdb.d/init.sql
+++ order-reception/tools/mysql/initdb.d/init.sql
@@ -1,20 +1,3 @@
-CREATE TABLE orders(
-        order_id varchar(250) PRIMARY KEY,
-        chain_id bigint NOT NULL,
-        shop_id bigint NOT NULL,
-        user_id bigint NOT NULL,
-        payment_method varchar(100) NOT NULL,
-        delivery_address_id bigint NOT NULL,
-        delivery_type varchar(100) NOT NULL CHECK (delivery_type IN ('immediate', 'specified')),
-        delivery_charge decimal NOT NULL,
-        non_taxed_total_price decimal NOT NULL,
-        tax decimal NOT NULL,
-        taxed_total_price decimal NOT NULL,
-        time datetime NOT NULL,
-        created_at datetime NOT NULL,
-        updated_at datetime NOT NULL
-);
-
 CREATE TABLE items(
         item_id bigint PRIMARY KEY AUTO_INCREMENT,
         chain_id bigint NOT NULL,
@@ -26,17 +9,6 @@ CREATE TABLE items(
         updated_at datetime NOT NULL
 --        FOREIGN KEY (chain_id) REFERENCES chains(chain_id),
 --        FOREIGN KEY (shop_id) REFERENCES shops(shop_id)
-);
-
-CREATE TABLE order_items(
-        order_id varchar(250) NOT NULL,
-        item_id bigint NOT NULL,
-        quantity integer NOT NULL,
-        created_at datetime NOT NULL,
-        updated_at datetime NOT NULL,
-        PRIMARY KEY (order_id, item_id),
-        FOREIGN KEY (order_id) REFERENCES orders(order_id),
-        FOREIGN KEY (item_id) REFERENCES items(item_id)
 );
 
 CREATE TABLE attributes(
@@ -57,31 +29,12 @@ CREATE TABLE item_attributes(
         FOREIGN KEY (attribute_id) REFERENCES attributes(attribute_id)
 );
 
-CREATE TABLE order_item_attributes(
-        order_id varchar(250) NOT NULL,
-        item_id bigint NOT NULL,
-        attribute_id bigint NOT NULL,
-        created_at datetime NOT NULL,
-        updated_at datetime NOT NULL,
-        PRIMARY KEY (order_id, item_id, attribute_id),
-        FOREIGN KEY (order_id) REFERENCES orders(order_id),
-        FOREIGN KEY (item_id) REFERENCES items(item_id),
-        FOREIGN KEY (attribute_id) REFERENCES attributes(attribute_id)
-);
-
-CREATE TABLE payment_methods(
-        name varchar(250) PRIMARY KEY,
-        created_at datetime NOT NULL,
-        updated_at datetime NOT NULL
-);
-
 CREATE TABLE ranks(
         rank_id bigint PRIMARY KEY AUTO_INCREMENT,
         name varchar(250) NOT NULL,
         created_at datetime NOT NULL,
         updated_at datetime NOT NULL
 );
-
 
 CREATE TABLE users(
         user_id bigint PRIMARY KEY AUTO_INCREMENT,
