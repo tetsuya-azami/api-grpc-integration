d1f7f6280221b6be452356f707cb69727b5c8edf
tetsuya-azami <zqianjian35@gmail.com>
2024-04-16 09:06:48 +0900

    add datasource settings in order-reception

--- order-reception/build.gradle.kts
+++ order-reception/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("net.devh:grpc-client-spring-boot-starter:3.0.0.RELEASE")
     implementation("com.michael-bull.kotlin-result:kotlin-result:2.0.0")
     implementation(project(":grpc-interface"))
+    implementation("com.mysql:mysql-connector-j:8.3.0")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
     implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.5.0")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
