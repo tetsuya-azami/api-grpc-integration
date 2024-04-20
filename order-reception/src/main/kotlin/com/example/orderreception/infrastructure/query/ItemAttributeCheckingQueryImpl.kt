65acc2427c49807ede3293ae1ba4b8bf47285af8
tetsuya-azami <zqianjian35@gmail.com>
2024-04-20 16:27:30 +0900

    fix ItemAttributeCheckingQuery

--- order-reception/src/main/kotlin/com/example/orderreception/infrastructure/query/ItemAttributeCheckingQueryImpl.kt
+++ order-reception/src/main/kotlin/com/example/orderreception/infrastructure/query/ItemAttributeCheckingQueryImpl.kt
@@ -1,8 +1,12 @@
 package com.example.orderreception.infrastructure.query
 
+import com.example.orderreception.error.ValidationError
 import com.example.orderreception.infrastructure.mapper.generated.ItemAttributesBaseMapper
 import com.example.orderreception.infrastructure.mapper.generated.selectOne
 import com.example.orderreception.usecase.query.ItemAttributeCheckingQuery
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
 import org.mybatis.dynamic.sql.util.kotlin.model.select
 import org.springframework.stereotype.Repository
 import com.example.orderreception.infrastructure.mapper.generated.ItemAttributesBaseDynamicSqlSupport as sqlSupport
@@ -11,7 +15,7 @@ import com.example.orderreception.infrastructure.mapper.generated.ItemAttributes
 class ItemAttributeCheckingQueryImpl(
     private val itemAttributesBaseMapper: ItemAttributesBaseMapper
 ) : ItemAttributeCheckingQuery {
-    override fun existsItemAttribute(itemId: Long, attributeId: Long): Boolean {
+    override fun existsItemAttribute(itemId: Long, attributeId: Long): Result<Boolean, ValidationError> {
         val attributesBase = itemAttributesBaseMapper.selectOne {
             select(sqlSupport.itemId) {
                 where {
@@ -23,6 +27,7 @@ class ItemAttributeCheckingQueryImpl(
             }
         }
 
-        return attributesBase != null
+        return if (attributesBase != null) Ok(true)
+        else Err(ValidationError(message = "商品情報と属性情報に整合性がありません。itemId=${itemId}, attributeId=${attributeId}"))
     }
 }
--- order-reception/src/main/kotlin/com/example/orderreception/usecase/query/ItemAttributeCheckingQuery.kt
+++ order-reception/src/main/kotlin/com/example/orderreception/usecase/query/ItemAttributeCheckingQuery.kt
@@ -1,5 +1,8 @@
 package com.example.orderreception.usecase.query
 
+import com.example.orderreception.error.ValidationError
+import com.github.michaelbull.result.Result
+
 interface ItemAttributeCheckingQuery {
-    fun existsItemAttribute(itemId: Long, attributeId: Long): Boolean
+    fun existsItemAttribute(itemId: Long, attributeId: Long): Result<Boolean, ValidationError>
 }
