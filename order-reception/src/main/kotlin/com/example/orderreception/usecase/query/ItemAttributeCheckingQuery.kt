cc722c4c0308638fe2b6b6fd19b88eb29bf62570
tetsuya-azami <zqianjian35@gmail.com>
2024-04-20 16:36:48 +0900

    fix ItemAttributeCheckingQuery

--- order-reception/src/main/kotlin/com/example/orderreception/usecase/query/ItemAttributeCheckingQuery.kt
+++ order-reception/src/main/kotlin/com/example/orderreception/usecase/query/ItemAttributeCheckingQuery.kt
@@ -1,8 +1,5 @@
 package com.example.orderreception.usecase.query
 
-import com.example.orderreception.error.ValidationError
-import com.github.michaelbull.result.Result
-
 interface ItemAttributeCheckingQuery {
-    fun existsItemAttribute(itemId: Long, attributeId: Long): Result<Boolean, ValidationError>
+    fun existsItemAttribute(itemId: Long, attributeId: Long): Boolean
 }
