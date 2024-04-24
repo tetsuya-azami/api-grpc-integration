b06260672eb739b44dff4a6be3dd7b46cff6af4e
tetsuya-azami <zqianjian35@gmail.com>
2024-04-24 09:25:17 +0900

    fix AddressQueryImpl query to use sql builder

--- order-reception/src/main/kotlin/com/example/orderreception/infrastructure/query/AddressQueryImpl.kt
+++ order-reception/src/main/kotlin/com/example/orderreception/infrastructure/query/AddressQueryImpl.kt
@@ -4,7 +4,8 @@ import com.example.orderreception.domain.model.order.Address
 import com.example.orderreception.infrastructure.mapper.generated.AddressesBaseMapper
 import com.example.orderreception.infrastructure.mapper.generated.selectOne
 import com.example.orderreception.usecase.query.AddressQuery
-import org.mybatis.dynamic.sql.util.kotlin.model.select
+import org.mybatis.dynamic.sql.SqlBuilder.isEqualTo
+import org.mybatis.dynamic.sql.SqlBuilder.select
 import org.springframework.stereotype.Repository
 import com.example.orderreception.infrastructure.mapper.generated.AddressesBaseDynamicSqlSupport as sqlSupport
 
@@ -14,14 +15,10 @@ class AddressQueryImpl(
 ) : AddressQuery {
     override fun findById(addressId: Long, userId: Long): Address? {
         val addressesBase = addressesBaseMapper.selectOne {
-            select(sqlSupport.addressId) {
-                where {
-                    sqlSupport.addressId isEqualTo addressId
-                    and {
-                        sqlSupport.userId isEqualTo userId
-                    }
-                }
-            }
+            select(sqlSupport.addressId)
+                .from(sqlSupport.addressesBase)
+                .where(sqlSupport.addressId, isEqualTo(addressId))
+                .and(sqlSupport.userId, isEqualTo(userId))
         }
 
         return addressesBase?.let { Address.fromBase(addressesBase) }
