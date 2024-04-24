b60291414f5b79bc7a945c7e92e93af0d647b64a
tetsuya-azami <zqianjian35@gmail.com>
2024-04-25 07:33:06 +0900

    add error handling for communication part with OrderProcessing

--- order-reception/src/main/kotlin/com/example/orderreception/infrastructure/internalapi/orderprocessing/client/OrderProcessingGrpcClient.kt
+++ order-reception/src/main/kotlin/com/example/orderreception/infrastructure/internalapi/orderprocessing/client/OrderProcessingGrpcClient.kt
@@ -7,18 +7,25 @@ import com.example.orderreception.infrastructure.internalapi.orderprocessing.req
 import com.example.orderreception.infrastructure.internalapi.orderprocessing.response.RegisterOrderResponse
 import com.example.orderreception.presentation.order.OrderParam
 import net.devh.boot.grpc.client.inject.GrpcClient
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class OrderProcessingGrpcClient(
     @GrpcClient("orderProcessingGrpcClient") private val orderServiceStub: OrderServiceBlockingStub
 ) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     fun registerOrder(orderParam: OrderParam, items: List<OrderItem>, user: User): RegisterOrderResponse {
         val registerOrderRequest = RegisterOrderRequest.fromModel(orderParam = orderParam, items = items, user = user)
 
         // TODO: 非同期での連携を検討
-        val orderCreationResponse = orderServiceStub.createOrder(registerOrderRequest.orderCreationRequest)
-
-        return RegisterOrderResponse(orderId = orderCreationResponse.id)
+        return try {
+            val orderCreationResponse = orderServiceStub.createOrder(registerOrderRequest.orderCreationRequest)
+            RegisterOrderResponse(orderId = orderCreationResponse.id)
+        } catch (e: Exception) {
+            logger.warn("OrderProcessingとの通信で予期せぬエラーが発生しました。", e)
+            throw e
+        }
     }
 }
