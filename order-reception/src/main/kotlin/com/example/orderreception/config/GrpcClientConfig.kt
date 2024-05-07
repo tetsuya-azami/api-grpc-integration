603aca4be9570dc848457d85e08e56fd03b57fbc
tetsuya-azami <zqianjian35@gmail.com>
2024-05-07 09:41:33 +0900

    implement grpc-client retry

---
+++ order-reception/src/main/kotlin/com/example/orderreception/config/GrpcClientConfig.kt
@@ -0,0 +1,27 @@
+package com.example.orderreception.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder
+import net.devh.boot.grpc.client.channelfactory.GrpcChannelConfigurer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.nio.file.Files
+import java.nio.file.Path
+
+
+@Configuration
+class GrpcClientConfig {
+    @Bean
+    fun grpcChannelConfigurer(): GrpcChannelConfigurer = GrpcChannelConfigurer { channelBuilder, name ->
+        if (channelBuilder is NettyChannelBuilder) {
+            val objectMapper = ObjectMapper()
+            val retryConfig: HashMap<String, Any> =
+                objectMapper.readValue(Files.readString(Path.of("src/main/resources/grpc-retry-config.json")))
+
+            channelBuilder
+                .defaultServiceConfig(retryConfig)
+                .enableRetry()
+        }
+    }
+}
