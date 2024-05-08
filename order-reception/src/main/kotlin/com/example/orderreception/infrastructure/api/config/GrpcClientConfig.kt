a7f4b4d31dd24da61ff60557f768d6ef197adb23
tetsuya-azami <zqianjian35@gmail.com>
2024-05-08 09:07:42 +0900

    mv grpc client config to infrastructure

--- order-reception/src/main/kotlin/com/example/orderreception/infrastructure/api/config/GrpcClientConfig.kt
+++ order-reception/src/main/kotlin/com/example/orderreception/infrastructure/api/config/GrpcClientConfig.kt
@@ -1,4 +1,4 @@
-package com.example.orderreception.config
+package com.example.orderreception.infrastructure.api.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
