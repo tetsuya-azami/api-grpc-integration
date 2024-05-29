6923d656f1449e25faaaa693098238764ee91b1e
tetsuya-azami <zqianjian35@gmail.com>
2024-05-30 08:25:29 +0900

    fix plugin version

--- order-reception/build.gradle.kts
+++ order-reception/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
     id("org.openapi.generator") version "7.2.0"
     id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
 }
