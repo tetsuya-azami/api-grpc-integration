40ef5374c15fd41fbaec28acf7f0b1d5b93d827a
tetsuya-azami <zqianjian35@gmail.com>
2024-05-29 09:21:33 +0900

    create merchant-administration

---
+++ merchant-administration/src/main/kotlin/com/example/merchant_administration/MerchantAdministrationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.merchant_administration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MerchantAdministrationApplication
+
+fun main(args: Array<String>) {
+	runApplication<MerchantAdministrationApplication>(*args)
+}
---
+++ merchant-administration/src/test/kotlin/com/example/merchant_administration/MerchantAdministrationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.merchant_administration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MerchantAdministrationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
